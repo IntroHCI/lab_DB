b58ae134c6836f479fc935b5944ff8760238a8d3
Jesse Qin <itzjesse@gmail.com>
2016-02-04 19:16:57 -0800

    beautifying code

--- models.js
+++ models.js
@@ -1,11 +1,8 @@
-
 var Mongoose = require('mongoose');
 
 
 var ProjectSchema = new Mongoose.Schema({
-  // fields are defined here
+    // fields are defined here
 });
 
 exports.Project = Mongoose.model('Project', ProjectSchema);
-
-
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,20 @@
-
 var models = require('../models');
 
 /*
  * GET home page.
  */
 
-exports.view = function(req, res){
+exports.view = function(req, res) {
 
-	models.Project
-		.find()
-		.sort('date')
-		.exec(renderProjects);
+    models.Project
+        .find()
+        .sort('date')
+        .exec(renderProjects);
 
-	function renderProjects(err, projects) {
-		res.render('index', { 'projects': projects });
-	}
+    function renderProjects(err, projects) {
+        res.render('index', {
+            'projects': projects
+        });
+    }
 
 };
